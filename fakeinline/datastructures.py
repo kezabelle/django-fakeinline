fda10178ec0ae7868bf7d1f087a575b74946486c
Keryn Knight <keryn@kerynknight.com>
2016-07-28 16:25:27 +0100

    Flesh out the fake inline some more.

--- fakeinline/datastructures.py
+++ fakeinline/datastructures.py
@@ -50,6 +50,15 @@ class FakeFormSet(object):
 
     errors = ()
 
+    # This is not required to fill out the API, but is useful for debugging ...
+    def __repr__(self):
+        instance = self.kwargs.get('instance', None)
+        return '<%(cls)s for obj: %(parent_instance)r, using form: %(form)r>' % {
+            'cls': self.__class__.__name__,
+            'parent_instance': instance,
+            'form': self.form,
+        }
+
 class InlineChecks(InlineModelAdminChecks):
     """ Wire up just enough to get through the system checks framework """
     def _check_relation(self, obj, parent_model):
@@ -63,5 +72,33 @@ class FakeInline(InlineModelAdmin):
     model = FakeModel
     checks_class = InlineChecks
     template = "admin/edit_inline/fakeinline.html"
+    min_num = 1
+    max_num = 1
+    # This is necessary to avoid needing to subclass BaseModelFormSet
+    # because of the system check.
+    fake_formset = FakeFormSet
+
     def get_formset(self, request, obj=None, **kwargs):
-        return FakeFormSet
+        # We're constructing a dynamic (unpicklable, sorry!) subclass
+        # and attaching all the admin data to it, so that the formset
+        # instance may do stuff.
+        parent_type = self.fake_formset.__name__
+        clsname = str('AdminAware%s' % parent_type)
+        extra_attrs = {
+            'admin_site': self.admin_site,
+            'parent_modeladmin': self.admin_site._registry[self.parent_model],
+            'request': request,
+            'instance': obj,
+        }
+        cls = type(clsname, (self.fake_formset,), extra_attrs)
+        return cls
+
+    def get_queryset(self, *args, **kwargs):
+        return () # QuerySet(model=self.model).none() causes issues.
+
+    # This is not required to fill out the API, but is useful for debugging ...
+    def __repr__(self):
+        return '<%(cls)s using formset: %(formset)r>' % {
+            'cls': self.__class__.__name__,
+            'formset': FakeFormSet,
+        }
--- fakeinline/models.py
+++ fakeinline/models.py
@@ -5,3 +5,4 @@ from django.db.models import Model
 class FakeModel(Model):
     class Meta:
         managed = False
+        abstract = True
